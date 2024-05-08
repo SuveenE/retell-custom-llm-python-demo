81e0f43c8c2c3826d3ca2a8dc20279ea6925a18b
SuveenE <suveen.te1@gmail.com>
2024-05-08 09:33:01 +0800

    Add test image prompt

--- app/server.py
+++ app/server.py
@@ -3,29 +3,19 @@ import os
 import asyncio
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
-from fastapi.responses import JSONResponse, PlainTextResponse
+from fastapi.responses import JSONResponse
 from concurrent.futures import TimeoutError as ConnectionTimeoutError
-from twilio.twiml.voice_response import VoiceResponse
 from retell import Retell
 from retell.resources.call import RegisterCallResponse
 from .custom_types import (
     ConfigResponse,
     ResponseRequiredRequest,
 )
-from .twilio_server import TwilioClient
 from .llm import LlmClient  # or use .llm_with_func_calling
 
 load_dotenv(override=True)
 app = FastAPI()
 retell = Retell(api_key=os.environ["RETELL_API_KEY"])
-
-# Custom Twilio if you want to use your own Twilio API Key
-twilio_client = TwilioClient()
-# twilio_client.create_phone_number(213, "68978b1c2935ff9c7d7107e61524d0bb")
-# twilio_client.delete_phone_number("+12133548310")
-# twilio_client.register_inbound_agent("+13392016322", "68978b1c2935ff9c7d7107e61524d0bb")
-# twilio_client.create_phone_call("+13392016322", "+14157122917", "68978b1c2935ff9c7d7107e61524d0bb")
-
 
 # Handle webhook from Retell server. This is used to receive events from Retell server.
 # Including call_started, call_ended, call_analyzed
@@ -61,49 +51,10 @@ async def handle_webhook(request: Request):
         )
 
 
-# Twilio voice webhook. This will be called whenever there is an incoming or outgoing call.
-# Register call with Retell at this stage and pass in returned call_id to Retell.
-@app.post("/twilio-voice-webhook/{agent_id_path}")
-async def handle_twilio_voice_webhook(request: Request, agent_id_path: str):
-    try:
-        # Check if it is machine
-        post_data = await request.form()
-        if "AnsweredBy" in post_data and post_data["AnsweredBy"] == "machine_start":
-            twilio_client.end_call(post_data["CallSid"])
-            return PlainTextResponse("")
-        elif "AnsweredBy" in post_data:
-            return PlainTextResponse("")
-
-        call_response: RegisterCallResponse = retell.call.register(
-            agent_id=agent_id_path,
-            audio_websocket_protocol="twilio",
-            audio_encoding="mulaw",
-            sample_rate=8000,  # Sample rate has to be 8000 for Twilio
-            from_number=post_data["From"],
-            to_number=post_data["To"],
-            metadata={
-                "twilio_call_sid": post_data["CallSid"],
-            },
-        )
-        print(f"Call response: {call_response}")
-
-        response = VoiceResponse()
-        start = response.connect()
-        start.stream(
-            url=f"wss://api.retellai.com/audio-websocket/{call_response.call_id}"
-        )
-        return PlainTextResponse(str(response), media_type="text/xml")
-    except Exception as err:
-        print(f"Error in twilio voice webhook: {err}")
-        return JSONResponse(
-            status_code=500, content={"message": "Internal Server Error"}
-        )
-
-
 # Only used for web call frontend to register call so that frontend don't need api key.
 # If you are using Retell through phone call, you don't need this API. Because
 # this.twilioClient.ListenTwilioVoiceWebhook() will include register-call in its function.
-@app.post("/register-call-on-your-server")
+@app.post("/register-call")
 async def handle_register_call(request: Request):
     try:
         post_data = await request.json()
@@ -116,6 +67,7 @@ async def handle_register_call(request: Request):
             ],  # Sample rate has to be 8000 for Twilio
         )
         print(f"Call response: {call_response}")
+        return call_response
     except Exception as err:
         print(f"Error in register call: {err}")
         return JSONResponse(
@@ -131,6 +83,7 @@ async def websocket_handler(websocket: WebSocket, call_id: str):
     try:
         await websocket.accept()
         llm_client = LlmClient()
+        print(llm_client)
 
         # Send optional config to Retell server
         config = ConfigResponse(
@@ -146,6 +99,7 @@ async def websocket_handler(websocket: WebSocket, call_id: str):
         # Send first message to signal ready of server
         response_id = 0
         first_event = llm_client.draft_begin_message()
+        print(first_event)
         await websocket.send_json(first_event.__dict__)
 
         async def handle_message(request_json):
